4e834f8ebe94e39ad591e39191946dee6fb5eef5
syoinGit <sasasyuga@yahoo.co.jp>
2026-05-01 19:42:56 +0900

    初回コミット

---
+++ src/main/java/yuki/creator_studio/CreatorStudioApplication.java
@@ -0,0 +1,13 @@
+package yuki.creator_studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yuki/creator_studio/CreatorStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package yuki.creator_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
